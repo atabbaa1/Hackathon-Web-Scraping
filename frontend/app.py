4a878b0a9cbbb2340809ac50846ee7ed81d30fa4
Adam Azeez <adambhav2623@gmail.com>
2024-07-28 16:09:18 +0300

    fixes opening files error

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 import os
 import sys
 original_sys_path = sys.path.copy()
@@ -40,6 +40,10 @@ def retrieve_text():
         return "Bill summary: " + text, 200
     except FileNotFoundError:
         return 'No text found', 404
+
+@app.route('/<path:filename>')
+def serve_file(filename):
+    return send_from_directory('templates', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
